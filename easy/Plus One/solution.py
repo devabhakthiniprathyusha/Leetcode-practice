b99efdcbbb40f06a0e682e72d88465f565b81c06
devabhakthiniprathyusha <devabhakthiniprathyusha@gmail.com>
2022-10-15 18:11:36 +0100

    Update solution.py

--- easy/Plus One/solution.py
+++ easy/Plus One/solution.py
@@ -3,23 +3,19 @@
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        n, carry = len(digits), 0
-        s = digits[n-1]+1
-        digits[n-1], carry = s%10, s//10
-        if not carry:
-            return digits
-        i = n-2
-        while i>=0 and carry:
-            s = digits[i]+carry
-            digits[i], carry = s%10, s//10
-            i-=1
-        if carry:
-            ans = []
-            while carry:
-                ans.append(carry%10)
-                carry//=10
-            ans = ans[::-1]
-            ans.extend(digits)
-            return ans
-        return digits
-        
+        # if the last digit is 9, carry forward and make carry 0
+        # inserting at first is two complicated so reverse the list in first
+        digits = digits[::-1]
+        carry, i = 1, 0
+        while carry:
+            if i < len(digits):
+                if digits[i] == 9:
+                    digits[i] = 0
+                else:
+                    digits[i] += 1
+                    carry = 0
+            else:
+                digits.append(1)
+                carry = 0
+            i += 1
+        return digits[::-1]
