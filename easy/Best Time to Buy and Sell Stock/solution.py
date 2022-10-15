7e9079af689db83c7191d2cec17a98b66eb66536
devabhakthiniprathyusha <devabhakthiniprathyusha@gmail.com>
2022-10-15 10:17:51 +0100

    Update solution.py

--- easy/Best Time to Buy and Sell Stock/solution.py
+++ easy/Best Time to Buy and Sell Stock/solution.py
@@ -3,16 +3,25 @@
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        n = len(prices)
-        minA, maxA = [10**4]*n, [0]*n
-        minA[0], maxA[n-1] = prices[0], prices[n-1]
-        if n==1:
-            return 0
-        for i in range(1, n):
-            minA[i] = min(prices[i], minA[i-1])
-        for i in range(n-2, -1, -1):
-            maxA[i] = max(prices[i], maxA[i+1])
-        ans = 0
-        for i in range(n):
-            ans = max(ans, abs(maxA[i]-minA[i]))
-        return ans
+        # peaks and lows in the prices
+        # keep track of minimum and change minimum price is found in the next iteration
+        # calculate the max_profit then
+        min_price = float('inf')
+        max_profit = 0
+        for i in range(len(prices)):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            elif prices[i] - min_price > max_profit:
+                max_profit = prices[i] - min_price
+        return max_profit
+
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        for i in range(len(prices) - 1):
+            for j in range(i + 1, len(prices)):
+                profit = prices[j] - prices[i]
+                if profit > max_profit:
+                    max_profit = profit
+        return max_profit
+
+
