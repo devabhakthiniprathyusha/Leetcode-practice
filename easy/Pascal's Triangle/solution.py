9701f8d4ee04c25910639f1d0af9ba0d44674287
devabhakthiniprathyusha <devabhakthiniprathyusha@gmail.com>
2022-10-16 17:16:55 +0100

    Update solution.py

--- easy/Pascal's Triangle/solution.py
+++ easy/Pascal's Triangle/solution.py
@@ -3,6 +3,19 @@
 
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
+        # adding zero one in front and one in end
+        # basically add the two number and create a new list nd append to res
+        res = [[1]]
+
+        for i in range(numRows - 1):
+            temp = [0] + res[-1] + [0]
+            row = []
+            for j in range(len(res[-1]) + 1):
+                row.append(temp[j] + temp[j + 1])
+            res.append(row)
+        return res
+
+    def generate(self, numRows: int) -> List[List[int]]:
         ans = [[1]]
         if numRows == 1:
             return ans
