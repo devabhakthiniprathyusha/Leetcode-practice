05ecae841dac0abe3dd076c6a46fdb879929c701
devabhakthiniprathyusha <devabhakthiniprathyusha@gmail.com>
2022-10-16 11:40:15 +0100

    Update solution.py

--- easy/Contains Duplicate/solution.py
+++ easy/Contains Duplicate/solution.py
@@ -1,9 +1,19 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        nums.sort()
+        nums.sort()  # sort - O(nlogn) with O(1)
         print(nums)
-        for i in range(len(nums)-1):
-            if nums[i]==nums[i+1]:
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
                 print(nums[i])
                 return True
         return False
+
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
+
