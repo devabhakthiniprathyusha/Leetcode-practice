c3563462a42b943bfb33d6d7e6bbc30c9afc8ebb
devabhakthiniprathyusha <devabhakthiniprathyusha@gmail.com>
2022-10-17 14:01:45 +0100

    Update solution.py

--- medium/Longest Substring Without Repeating Characters/solution.py
+++ medium/Longest Substring Without Repeating Characters/solution.py
@@ -3,6 +3,20 @@
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        charSet = set()
+        l = 0
+        res = 0
+
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+
+            charSet.add(s[r])
+            res = max(res, r - l + 1)
+        return res
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
         if len(s) == 0:
             return 0
         mp = {}
@@ -19,5 +33,44 @@ class Solution:
                     j+=1
             i+=1
         return mx
-                
-                
+
+    class Solution:
+        # O(2n) space- O(min(m,n))
+        def lengthOfLongestSubstring(self, s: str) -> int:
+            chars = Counter()
+
+            left = right = 0
+
+            res = 0
+            while right < len(s):
+                r = s[right]
+                chars[r] += 1
+
+                while chars[r] > 1:
+                    l = s[left]
+                    chars[l] -= 1
+                    left += 1
+
+                res = max(res, right - left + 1)
+
+                right += 1
+            return res
+
+        # O(n) space- O(min(m,n))
+        def lengthOfLongestSubstring(self, s: str) -> int:
+            n = len(s)
+            ans = 0
+            # mp stores the current index of a character
+            mp = {}
+
+            i = 0
+            # try to extend the range [i, j]
+            for j in range(n):
+                if s[j] in mp:
+                    i = max(mp[s[j]], i)
+
+                ans = max(ans, j - i + 1)
+                mp[s[j]] = j + 1
+
+            return ans
+
