01aba20d386dfb6b6614322123f28045ffd2b103
devabhakthiniprathyusha <devabhakthiniprathyusha@gmail.com>
2022-10-20 11:25:15 +0100

    Update solution.py

--- medium/Longest Consecutive Sequence/solution.py
+++ medium/Longest Consecutive Sequence/solution.py
@@ -2,12 +2,51 @@
 # Approach: Take streak and keep incrementing the number in set.
 
 class Solution:
-    def longestConsecutive(self, nums: List[int]) -> int:
-        ls, nums = 0, set(nums)
+    # brute force O(n**3)
+    def longestConsecutive(self, nums):
+        longest_streak = 0
+
         for num in nums:
-            if num-1 not in nums:
-                cn, cs = num, 1
-                while cn+1 in nums:
-                    cn, cs = cn+1, cs+1
-                ls = max(ls, cs)
-        return ls
+            current_num = num
+            current_streak = 1
+
+            while current_num + 1 in nums:
+                current_num += 1
+                current_streak += 1
+
+            longest_streak = max(longest_streak, current_streak)
+
+        return longest_streak
+
+    # sorting O(nlogn)
+    def longestConsecutive(self, nums):
+        if not nums:
+            return 0
+
+        nums.sort()
+
+        longest_streak = 1
+        current_streak = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                if nums[i] == nums[i - 1] + 1:
+                    current_streak += 1
+                else:
+                    longest_streak = max(longest_streak, current_streak)
+                    current_streak = 1
+
+        return max(longest_streak, current_streak)
+
+    # hashSet O(n)
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+
+        for n in nums:
+            if n - 1 not in numSet:
+                length = 0
+                while n + length in numSet:
+                    length += 1
+                longest = max(longest, length)
+        return longest
